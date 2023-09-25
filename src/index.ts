e1d1f74054638afbc4f0b1cd7d5c300d19047c53
palmcm <palmermonkeyboy@gmail.com>
2023-09-25 14:13:01 +0700

    try to fix cors2

--- src/index.ts
+++ src/index.ts
@@ -4,9 +4,8 @@ import { cors } from "@elysiajs/cors";
 
 const app = new Elysia().use(swagger()).use(
   cors({
-    origin: "http://127.0.0.1:5173",
+    origin: ["https://intelliseat.pkhing.dev", "http://127.0.0.1:5173"],
     credentials: true,
-    maxAge: 3600,
   })
 );
 
