9ffe54d2cfd40fe0d3d333ddbf89d2bb43f623c2
palmcm <palmermonkeyboy@gmail.com>
2023-10-01 16:11:30 +0700

    not add data in the same time

--- src/service/logtodb.ts
+++ src/service/logtodb.ts
@@ -2,36 +2,42 @@ import { Side } from "@prisma/client";
 import prisma from "../prisma";
 import { SIT_THRESHOLD, SIT_TIME_NOTIFICATION } from "../constant";
 
+const addLogToDB = async (
+  nodeGroup: string,
+  sensor: { nodeSide: Side; weight: number },
+  Datenoms: Date
+) => {
+  await prisma.log.create({
+    data: {
+      weight: sensor.weight,
+      node: {
+        connectOrCreate: {
+          where: {
+            nodeGroup_nodeSide: {
+              nodeGroup: nodeGroup,
+              nodeSide: sensor.nodeSide,
+            },
+          },
+          create: {
+            nodeGroup: nodeGroup,
+            nodeSide: sensor.nodeSide,
+          },
+        },
+      },
+      logged_at: Datenoms,
+    },
+  });
+};
+
 export const logToDB = async (
   nodeGroup: string,
   data: { nodeSide: Side; weight: number }[]
 ) => {
   const now = Date.now();
   const Datenoms = new Date(now - (now % 1000));
-  await Promise.all(
-    data.map(async (sensor) => {
-      await prisma.log.create({
-        data: {
-          weight: sensor.weight,
-          node: {
-            connectOrCreate: {
-              where: {
-                nodeGroup_nodeSide: {
-                  nodeGroup: nodeGroup,
-                  nodeSide: sensor.nodeSide,
-                },
-              },
-              create: {
-                nodeGroup: nodeGroup,
-                nodeSide: sensor.nodeSide,
-              },
-            },
-          },
-          logged_at: Datenoms,
-        },
-      });
-    })
-  );
+  for (let i = 0; i < data.length; i++) {
+    await addLogToDB(nodeGroup, data[i], Datenoms);
+  }
   let weight = data.map((sensor) => sensor.weight).reduce((a, b) => a + b, 0);
   if (weight < SIT_THRESHOLD) return;
   const startSit = await prisma.tempLog.findFirst({
