d9c5b242601b0d7bd1e1b1af6232db07ecf6168c
palmcm <palmermonkeyboy@gmail.com>
2023-10-02 22:17:55 +0700

    hotfix

--- src/constant.ts
+++ src/constant.ts
@@ -2,4 +2,4 @@ export const SIT_THRESHOLD = 10000; // Define your threshold for detecting a per
 export const SENSOR_THRESHOLD = 4000; // Define your threshold for sensor difference
 export const SIT_TIME_NOTIFICATION = 1; // Define your threshold for notification in minutes
 export const BAD_POSITION_TIME = 60 * 1; // Define your threshold for notification in seconds
-export const STAND_UP_TIME = 30; // Define your threshold for notification in seconds
+export const STAND_UP_TIME = 1; // Define your threshold for notification in seconds
