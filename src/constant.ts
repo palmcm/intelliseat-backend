736a9413fd013acfe77aa0a270814ef12e15f9c9
palmcm <palmermonkeyboy@gmail.com>
2023-10-02 13:53:15 +0700

    edit constant

--- src/constant.ts
+++ src/constant.ts
@@ -1,5 +1,5 @@
 export const SIT_THRESHOLD = 3000; // Define your threshold for detecting a person sitting
 export const SENSOR_THRESHOLD = 1000; // Define your threshold for sensor difference
-export const SIT_TIME_NOTIFICATION = 60 * 5; // Define your threshold for notification in seconds
-export const BAD_POSITION_TIME = 5; // Define your threshold for notification in seconds
+export const SIT_TIME_NOTIFICATION = 1; // Define your threshold for notification in minutes
+export const BAD_POSITION_TIME = 60 * 5; // Define your threshold for notification in seconds
 export const STAND_UP_TIME = 30; // Define your threshold for notification in seconds
