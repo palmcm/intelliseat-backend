ffaa910f5fd87df736ca71ab895cb2f510801048
palmcm <palmermonkeyboy@gmail.com>
2023-10-02 23:41:12 +0700

    debug5

--- src/service/daysdata.ts
+++ src/service/daysdata.ts
@@ -27,6 +27,15 @@ export const daysData = async (nodeGroup: string) => {
     if (days[date] == null) {
       days[date] = 0;
     }
+    if (timeLog.logged_at.getDay() < 1 || timeLog.logged_at.getMonth() < 9) {
+      console.log(
+        date,
+        last_logged_at[date],
+        timeLog.logged_at.getDay(),
+        timeLog.logged_at.getMonth()
+      );
+    }
+
     if (
       last_logged_at[date] != null &&
       (timeLog.logged_at.getDay() < 1 || timeLog.logged_at.getMonth() < 9)
